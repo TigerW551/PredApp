7d9ce4ae5f3e8fc0a104b3a5572bfea93f3da73f
Marek Zieliński <marozet@gmail.com>
2015-02-23 03:37:01 +0100

    finally working in shinyapps.io

--- server.R
+++ server.R
@@ -9,9 +9,14 @@
 
 library(shiny)
 library(ROCR)
+library(gbm)
+library(nnet)
+library(randomForest)
+library(e1071)
 library(caret)
 #prepare dataset
-red.wine<-read.csv("winequality-red.csv",sep=";")
+red.wine<-read.csv("data/winequality-red.csv",sep=";")
+#red.wine<-read.csv("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv",sep=";")
 red.wine$quality2<-0
 red.wine$quality2[red.wine$quality>=6]<-1 #consider wines scored as 6 or above as good
 red.wine$quality2<-as.factor(red.wine$quality2)
@@ -19,31 +24,63 @@ levels(red.wine$quality2)<-c("mediocre","good")
 
 shinyServer(function(input, output) {
 
-    inTrain <- reactive({set.seed(input$seed);inTrain <- createDataPartition(red.wine$quality2,p=0.75,list=FALSE)})
+    # split data into training and testing
+    inTrain <- reactive({set.seed(input$seed);inTrain <- createDataPartition(red.wine$quality2,p=input$split,list=FALSE)})
     training <- reactive({
         inTrain <- inTrain()
         training<-red.wine[inTrain,-grep("quality$",names(red.wine))]
     })
+    
+    # count training data
+    output$n_training <- renderText({
+        training <- training()
+        nrow(training)
+    })
+    
     testing <- reactive({
         inTrain <- inTrain()
         testing<-red.wine[-inTrain,-grep("quality$",names(red.wine))]
     })
+
+    # count test data
+    output$n_testing <- renderText({
+        testing <- testing()
+        nrow(testing)
+    })
     
+    output$method <-renderText({
+        switch(input$method,
+               gbm = "Stochastic Gradient Boosting (gbm)",
+               rf = "Random Forrest (rf)",
+               nnet = "Neural Network (nnet)"
+               )
+    })
+
+    output$cv_k <- renderText({
+        input$cv_k
+    })
+    
+    output$pred_tresh <- renderText({
+        input$pred_tresh
+    })
+    
+    #train the model
     model <- reactive({
         training <- training()
         ctrl <- trainControl(method = "cv",number=input$cv_k)
-        model<-train(quality2~.,data=training,method="gbm",verbose=FALSE,trControl=ctrl)
+        model<-train(quality2~.,data=training,method=input$method,verbose=FALSE,trControl=ctrl)
     })
     
+    # calculate prediction probabilities
     pred <- reactive({
         model <- model()
         testing <- testing()
         pred<-predict(model,testing,type="prob")
     })
     
-    output$distPlot <- renderPlot({
+    output$ROCPlot <- renderPlot({
 
-      # run predictions
+      # create ROC plot
  
       testing <- testing()  
       pred <- pred()
@@ -53,11 +90,15 @@ shinyServer(function(input, output) {
 
     # draw the ROC curve
     plot(ROCperf,main="ROC Curve")
+    
+    # put treshold point on the plot
     x<-ROCperf@x.values[[1]][ROCperf@x.values[[1]]>input$pred_tresh][1]
     x_index<-which(ROCperf@x.values[[1]]==x)[1]
     y<-ROCperf@y.values[[1]][x_index]
     points(x,y,pch=4,cex=3,col="blue",lw=3)
   })
+  
+  #calculate confusion Matrix
   confMatrix <- reactive({
       input$pred_tresh
       pred <- pred()
--- ui.R
+++ ui.R
@@ -17,6 +17,14 @@ shinyUI(fluidPage(
     sidebarPanel(
         helpText("You can change learning option here:"),
       numericInput("seed", "Set seed:", 123456789),
+      sliderInput("split",
+                  "Select split point for training and test data:",
+                  min = 0.05,
+                  max = 1,
+                  value = 0.75),
+      selectInput("method", "Select learning method", 
+                  choices = list("Stochastic Gradient Boosting" = "gbm", "Random Forrest" = "rf",
+                                 "Neural Network" = "nnet"), selected = "gbm"),
       sliderInput("cv_k",
                   "select k for k-fold cross validation:",
                   min = 2,
@@ -24,24 +32,39 @@ shinyUI(fluidPage(
                   value = 2),
       sliderInput("pred_tresh",
                   "select treshold probability for prediction:",
-                  min = 0.001,
-                  max = 0.999,
-                  value = 0.5)
+                  min = 0,
+                  max = 1,
+                  value = 0.5),
+      br(),
+      h5("Cases in training data:"),
+      verbatimTextOutput("n_training"),
+      h5("Cases in test data:"),
+      verbatimTextOutput("n_testing"),
+      h5("Method selected:"),
+      verbatimTextOutput("method"),
+      h5("k-fold cross validation:"),
+      verbatimTextOutput("cv_k"),
+      h5("Probability treshold:"),
+      verbatimTextOutput("pred_tresh")     
     ),
 
     # Show a plot of the generated distribution
     mainPanel(
-
-        helpText("This app attempts to learn how to distinguish red wine quality based on physicochemical tests. The data is taken from UCI Machine Learning Repository and is related to red vinho verde wine samples, from the north of Portugal. You can view the description of the data and download the original file ",a("[here]", href="http://archive.ics.uci.edu/ml/datasets/Wine+Quality",target="_blank")),
-        plotOutput("distPlot"),
-        h5("Confusion Matrix"),
-        tableOutput("confusionMatrixTable"),
-        helpText("rows - prediction, columns - reference"),
-        h5("Performance measures"),
-        verbatimTextOutput("cfAccuracy"),
-        verbatimTextOutput("cfSensitivity"),
-        verbatimTextOutput("cfSpecificity"),
-        verbatimTextOutput("cfBalanced")
+        tabsetPanel(
+            tabPanel("Prediction",
+                helpText("This app attempts to learn how to distinguish red wine quality based on physicochemical tests. The data is taken from UCI Machine Learning Repository and is related to red vinho verde wine samples, from the north of Portugal. You can view the description of the data and download the original file ",a("[here]", href="http://archive.ics.uci.edu/ml/datasets/Wine+Quality",target="_blank"),". Please be patient while the app initializes."),
+                plotOutput("ROCPlot"),
+                h5("Confusion Matrix"),
+                tableOutput("confusionMatrixTable"),
+                helpText("rows - prediction, columns - reference"),
+                h5("Performance measures"),
+                verbatimTextOutput("cfAccuracy"),
+                verbatimTextOutput("cfSensitivity"),
+                verbatimTextOutput("cfSpecificity"),
+                verbatimTextOutput("cfBalanced")
+            ),
+            tabPanel("Documentation",includeHTML("PredAppDocumentation.html"))
+        )
         #verbatimTextOutput("confusionMatrixTable")
     )
   )
